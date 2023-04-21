9611bfa595350ae763d809fc5e96f1c04df034f7
AneeshRijhwani25 <7851950798aneesh@gmail.com>
2023-04-21 23:57:55 +0530

    Created controller Class and installed rabbitMq in the device

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -1,26 +1,28 @@
 const { BookingService } = require('../services/index');
 const { StatusCodes } = require('http-status-codes');
 const bookingService = new BookingService();
-const create = async(req,res) => { 
-    try {
-        const response = await bookingService.createBooking(req.body);
-        return res.status(StatusCodes.OK).json({
-            message: 'Successfully Completed Booking',
-            success: true,
-            err: {},
-            data: response
-        });
-    } catch (error) {
-        return res.status(error.statusCode).json({
-            message: error.message,
-            success: false,
-            err: error.explanation,
-            data: {}
-        });
+
+class BookingContoller {
+    async create (req,res) { 
+        try {
+            const response = await bookingService.createBooking(req.body);
+            return res.status(StatusCodes.OK).json({
+                message: 'Successfully Completed Booking',
+                success: true,
+                err: {},
+                data: response
+            });
+        } catch (error) {
+            return res.status(error.statusCode).json({
+                message: error.message,
+                success: false,
+                err: error.explanation,
+                data: {}
+            });
+        }
     }
 }
 
 
-module.exports = {
-    create
-}
+
+module.exports = BookingContoller;
