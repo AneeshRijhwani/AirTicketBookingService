77b337ae61ea2ac9dc9f1096297ea112b4ef6374
AneeshRijhwani25 <7851950798aneesh@gmail.com>
2023-04-30 22:48:33 +0530

    Modified for API_Gateways

--- src/index.js
+++ src/index.js
@@ -7,8 +7,10 @@ const {PORT} = require('./config/serverConfig');
 const setupAndStartServer = ()=>{
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
-
-    app.use('/api',apiRoutes);
+    // app.get('/bookingservice/api/v1/home', (req,res) => {
+    //     return res.json({message: 'Hitting booking Service'});
+    // });
+    app.use('/bookingservice/api',apiRoutes);
     app.listen(PORT,()=>{
         console.log('Server Started At :',PORT);
         if(process.env.DB_SYNC){
